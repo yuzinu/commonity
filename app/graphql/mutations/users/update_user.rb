e35ce122392b4a068ffdef301ea834a420d6cb93
junidaddy <moonyoojin@gmail.com>
2021-07-28 19:43:31 -0400

    Modify resolve for UpdateUser mutation

--- app/graphql/mutations/users/update_user.rb
+++ app/graphql/mutations/users/update_user.rb
@@ -5,27 +5,15 @@ module Mutations
       argument :username, String, required: false
       argument :email, String, required: false
       
-      field :user, Types::UserType, null: false
-      field :errors, [String], null: false
+      type Types::UserType
 
       def resolve(id:, username: nil, email: nil)
         user = User.find(id)
         if user
           user.username = username unless username.nil?
           user.email = email unless email.nil?
-
-          if user.save
-            {
-              user: user,
-              errors: []
-            }
-          else
-            {
-              user: user,
-              errors: user.errors.full_messages
-            }
-          end
         end
+        return user if user.save
       end
     end
   end
