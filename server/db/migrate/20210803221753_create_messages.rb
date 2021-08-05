5fba330ff7d40447e520447e39446551623b916f
junidaddy <moonyoojin@gmail.com>
2021-08-05 14:13:44 -0400

    Reconfigure schema to add auth with devise

--- server/db/migrate/20210803221753_create_messages.rb
+++ server/db/migrate/20210803221753_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration[6.1]
       t.text :body, null: false
       t.references :author, null: false
       t.references :messageable, polymorphic: true, null: false
-
+      
       t.timestamps
     end
   end
